cd61d06912e2cb838a649209842b914cb6c02454
yasuyasu-ot <y.otsubo.dss@gmail.com>
2020-01-19 02:27:46 +0900

    要件2,4の実装

--- contacts_app/config/routes.rb
+++ contacts_app/config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  get 'contacts/new'
-  get 'contacts/create'
+  resources :contacts, only: [:new, :create]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
