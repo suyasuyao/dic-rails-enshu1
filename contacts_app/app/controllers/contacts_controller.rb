0b5cc14e524dbfd3ca538d81c2f17ee719887b9e
yasuyasu-ot <y.otsubo.dss@gmail.com>
2020-01-19 19:20:51 +0900

    要件３　newアクションでフォーム作成し、createで保存し,newでredirect

--- contacts_app/app/controllers/contacts_controller.rb
+++ contacts_app/app/controllers/contacts_controller.rb
@@ -4,5 +4,10 @@ class ContactsController < ApplicationController
   end
 
   def create
+    @contact = Contact.new(name: params[:contact][:name],
+                           email: params[:contact][:email],
+                           content: params[:contact][:content])
+    @contact.save
+    redirect_to new_contact_path
   end
 end
